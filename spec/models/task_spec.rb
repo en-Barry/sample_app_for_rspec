f092893180510a373378d75afd8b810a4284185c
en-Barry <en_barry@icloud.com>
2020-12-13 17:05:51 +0900

    Fix: taskネームの訂正とvalid?からbe_invalidへの変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,31 +5,33 @@ RSpec.describe Task, type: :model do
     it 'is valid with all attrbutes' do
       task = build(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
-      task = build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'is invalid without status' do
-      task = build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to eq ["can't be blank"]
     end
 
     it 'is invalid with a duplicate title' do
-      create(:task)
-      task = build(:task)
-      task.valid?
-      expect(task.errors[:title]).to include('has already been taken')
+      task = create(:task)
+      task_with_duplicate_title = build(:task, title: task.title)
+      expect(task_with_duplicate_title).to be_invalid
+      expect(task_with_duplicate_title.errors[:title]).to eq ['has already been taken']
     end
 
     it 'is valid with another title' do
-      create(:task)
-      task = build(:task, title: 'sample02')
-      expect(task).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'sample02')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors[:title]).to be_empty
     end
   end
 end
